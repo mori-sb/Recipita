d8805bf0671680f21b2438cbcfe5cb4e8ad80ef7
mori-sb <kazuma.morishita.6d@gmail.com>
2025-06-10 19:43:25 +0900

    modify-key

--- springboot-api/src/main/java/com/recipita/config/WebConfig.java
+++ springboot-api/src/main/java/com/recipita/config/WebConfig.java
@@ -9,8 +9,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedOrigins("https://next-client-95z1.onrender.com")// ReactのURL
+                .allowedOrigins("http://localhost:3000",
+                        "https://next-client-95z1.onrender.com")// ReactのURL
                 .allowedMethods("*");
     }
 }
